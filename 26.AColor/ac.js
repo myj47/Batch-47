687877526b80a77b5e9dd8d07a1c0e066262b3cf
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:29:53 +0500

    Add files via upload

---
+++ 26.AColor/ac.js
@@ -0,0 +1,7 @@
+var alien_color = "green";
+if (alien_color === "yellow") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("The player just earned 10 points for earning the alien");
+}
---
+++ 26.AColor/ac.ts
@@ -0,0 +1,11 @@
+
+let alien_color:string="green"
+if(alien_color==="yellow"){
+    console.log("The player just earned 5 points for shooting the alien");
+}
+if(alien_color==="yellow"){
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else{
+    console.log("The player just earned 10 points for earning the alien");
+}
