a2444810e6ba3970bccba623ec03268b30cefbe2
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:28:25 +0500

    Add files via upload

---
+++ 22.Earr/er.js
@@ -0,0 +1,6 @@
+var arr = ["Apple", "Mango", "Orange"];
+console.log(arr);
+console.log(arr[3]);
+arr.push("Peach");
+console.log(arr[3]);
+console.log(arr);
---
+++ 22.Earr/er.ts
@@ -0,0 +1,6 @@
+let arr=["Apple","Mango","Orange"];
+console.log(arr);
+console.log(arr[3]);
+arr.push("Peach");
+console.log(arr[3]);
+console.log(arr);
