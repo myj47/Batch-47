efbde536f1c6589158b266e3e1f20e24e1889294
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:29:33 +0500

    Add files via upload

---
+++ 25.Alien/al.js
@@ -0,0 +1,10 @@
+var alien_name = "green";
+if (alien_name == "green") {
+    console.log("The player just earned 5 points");
+}
+if (alien_name == "yellow") {
+    console.log("The player just earned 5 points");
+}
+if (alien_name == "green") {
+    console.log("The player just earned 5 points");
+}
---
+++ 25.Alien/al.ts
@@ -0,0 +1,10 @@
+let alien_name:string="green";
+if(alien_name=="green"){
+console.log("The player just earned 5 points")
+}
+if(alien_name=="yellow"){
+    console.log("The player just earned 5 points")
+}
+if(alien_name=="green"){
+    console.log("The player just earned 5 points")
+    }
