dc1198c6b71ca72b2af037d9862d7dcf9ecf5aa0
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:35:18 +0500

    Add files via upload

---
+++ 36.Shirt/sh.js
@@ -0,0 +1,5 @@
+function make_shirt(size, text) {
+    return ("The size of the shirt is ".concat(size, " , ").concat(text));
+}
+;
+console.log(40, "Long live Pakistan");
---
+++ 36.Shirt/sh.ts
@@ -0,0 +1,4 @@
+function make_shirt(size:number,text:string){
+    return(`The size of the shirt is ${size} , ${text}`)
+};
+console.log(40,"Long live Pakistan")
