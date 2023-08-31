c5ff30514557bcfafc0903203ceb2599d7c1bdf6
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:31:39 +0500

    Add files via upload

---
+++ 29.Fruit/fr.js
@@ -0,0 +1,16 @@
+var favourite_fruits = ["Apple", "Mango", "Orange", "Banana"];
+if (favourite_fruits.includes("Apple")) {
+    console.log("You really like Apple");
+}
+if (favourite_fruits.includes("Mango")) {
+    console.log("You really like Mango");
+}
+if (favourite_fruits.includes("Orange")) {
+    console.log("You really like Orange");
+}
+if (favourite_fruits.includes("Banana")) {
+    console.log("You really like Banana");
+}
+if (favourite_fruits.includes("Peach")) {
+    console.log("You really like Peach");
+}
