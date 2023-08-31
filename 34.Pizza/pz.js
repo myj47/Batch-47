8534e18e77a9b75f816d5d5921972b5cbb251ba2
Muhammad Yasir Jilani <138642748+myj47@users.noreply.github.com>
2023-08-31 19:34:38 +0500

    Add files via upload

---
+++ 34.Pizza/pz.js
@@ -0,0 +1,5 @@
+var favoritePizzas = ["pepperoni", "cheese", "mushroom"];
+for (var i = 0; i < favoritePizzas.length; i++) {
+    console.log("I like".concat(favoritePizzas[i], " pizza"));
+}
+console.log("t is one of my favorite foods. I love the combination of the different flavors and textures, and I love the fact that there are so many different kinds of pizza to choose from. I could eat pizza every day and never get tired of it.I really love pizza!");
